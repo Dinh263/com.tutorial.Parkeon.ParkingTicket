791bf8f93ddb04678d8007f1a68e6a251bae3815
Dinh263 <dinh.test263@hotmail.com>
2017-10-27 03:24:34 -0700

    upload code lan 2
    commit 2nd

--- src/test/java/Testcases/TC01_Login.java
+++ src/test/java/Testcases/TC01_Login.java
@@ -1,5 +1,8 @@
 package Testcases;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.GeckoDriverService;
@@ -7,6 +10,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import CustomizedLibrary.PropertyUtility;
 import Pages.ChildPages.IndexPage;
 
 public class TC01_Login {
@@ -15,10 +19,12 @@ public class TC01_Login {
 	WebDriver driver;
 	
 	@BeforeTest
-	public void openBrowserAndGoToWebsite() {
-		
+	public void openBrowserAndGoToWebsite() throws IOException {
+		System.setProperty("webdriver.gecko.driver",PropertyUtility.getGeckoDriverPath());
 		driver = new FirefoxDriver();
-		indexobj = new IndexPage(driver);
+		indexobj =  new IndexPage(driver);
+		indexobj.setPageUrl();
+		
 	}
 	
 	@Test
